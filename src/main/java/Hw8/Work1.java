c1120899417548c5358c66fd934237c9ab26577b
mykola66 <nsudenkov@hotmail.com>
2021-03-08 23:42:29 -0500

    Hw 8 from 3.6.21

---
+++ src/main/java/Hw8/Work1.java
@@ -0,0 +1,40 @@
+package Hw8;
+
+import java.util.Arrays;
+
+public class Work1 {
+    public static void main(String[] args) {
+        double sum = getSum(2, 3, 5, 6, 7, 8);
+        System.out.println(sum);
+
+        int[] arr = {3, 9, 1, 8, 4, 8, 5};
+        int min = getMinFromArray(arr);
+        System.out.println("Array min =` " + min);
+
+        int[] array = {3, 9, 1, 8, 4, 8, 5};
+        Arrays.sort(array);
+        int i = 0;
+        for (int v : array) {
+            System.out.println(array[i]);
+            i++;
+        }
+
+    }
+
+    public static double getSum(int a, int b, int c, int d, int e, int f) {
+        double sum = a + b + c + d + e + f;
+        return sum;
+    }
+
+    public static int getMinFromArray(int[] array) {
+        int m = array[0];
+        for (int v : array) {
+            if (v < m) ;
+        }
+        return m;
+    }
+
+    public static void sort(int[] array) {
+
+    }
+}
