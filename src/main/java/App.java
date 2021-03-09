4117f5c8f0bb5b1678fd46a4da86a91527f598f7
greg <klingergk@yahoo.com>
2021-03-08 20:09:21 -0800

    Initial commit..

--- src/main/java/App.java
+++ src/main/java/App.java
@@ -2,5 +2,6 @@ public class App {
     public static void main(String[] args) {
         System.out.println("Hello, world");
         System.out.println("We worked hard and learn some new stuff");
+        System.out.println("This is a new line");
     }
 }
