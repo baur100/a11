663302708b138f629b1c5b8a15e9dbe524e17d5e
baur <baurzhan.zh@gmail.com>
2021-03-29 23:08:29 -0400

    JavaSelenium - Lesson 6 - POM 1

---
+++ src/test/java/pageObjectTests/PlaylistTests.java
@@ -0,0 +1,37 @@
+package pageObjectTests;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import pageObjects.LoginPage;
+import pageObjects.MainPage;
+
+public class PlaylistTests {
+    private WebDriver driver;
+    @BeforeMethod
+    public void startUp(){
+        WebDriverManager.chromedriver().setup();
+        driver = new ChromeDriver();
+
+    }
+    @AfterMethod
+    public void tearDown() throws InterruptedException {
+        Thread.sleep(3000);
+        driver.quit();
+    }
+    @Test
+    public void playlistTest_createPlaylist_playlistCreated(){
+        String playlistName = "XX";
+        LoginPage loginPage = new LoginPage(driver);
+        loginPage.open();
+        MainPage mainPage = loginPage.loginToApp("koeluser06@testpro.io","te$t$tudent");
+        String playlistId = mainPage.createPlaylist(playlistName);
+        Assert.assertTrue(mainPage.isPlaylistExist(playlistId,playlistName));
+
+    }
+
+}
