a611210cb8f9b19761d2c53d242857c8f93ba5db
greg <klingergk@yahoo.com>
2021-04-17 09:56:50 -0700

    Testing try catch.

---
+++ src/main/java/gpkTestPackage041721A/mainTest041721.java
@@ -0,0 +1,8 @@
+package gpkTestPackage041721A;
+
+public class mainTest041721 {
+    public static void main(String[] args) {
+        System.out.println("Hi There");
+        System.out.println("This is a test");
+    }
+}
