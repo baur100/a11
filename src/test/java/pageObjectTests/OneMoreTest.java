8ffd34a410b871d0d160c6ca1154afb123b073c3
baur <baurzhan.zh@gmail.com>
2021-04-16 22:32:02 -0400

    ZZ-1234 Addressed comments

--- src/test/java/pageObjectTests/OneMoreTest.java
+++ src/test/java/pageObjectTests/OneMoreTest.java
@@ -8,16 +8,16 @@ import pageObjects.MainPage;
 
 public class OneMoreTest extends BaseTest {
     @Test
-    public void playlistTest_renamePlaylist_playlistRenamedxx(){
-        Faker faker = new Faker();
-        String playlistName = faker.funnyName().name();
+    public void oh_iRenamedTheTest(){
+        var faker = new Faker();
+        var playlistName = faker.funnyName().name();
 
-        LoginPage loginPage = new LoginPage(driver);
+        var loginPage = new LoginPage(driver);
         loginPage.open(url);
-        MainPage mainPage = loginPage.loginToApp(username,password);
-        String playlistId = mainPage.createPlaylist(playlistName);
+        var mainPage = loginPage.loginToApp(username,password);
+        var playlistId = mainPage.createPlaylist(playlistName);
 
-        String newName = faker.ancient().hero();
+        var newName = faker.ancient().hero();
         mainPage.renamePlaylist(playlistId,newName);
 
         Assert.assertTrue(mainPage.isPlaylistExist(playlistId,newName));
