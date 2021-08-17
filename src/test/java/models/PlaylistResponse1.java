97372cbfffe7905ab79f2c2126dde163ec2fafb8
Joseph Jen <jjen.mis@gmail.com>
2021-08-16 22:22:02 -0500

    Added Jo's PlaylistResponse1.java

---
+++ src/test/java/models/PlaylistResponse1.java
@@ -0,0 +1,14 @@
+package models;
+
+public class PlaylistResponse1 {
+    private String name;
+    private int id;
+
+    public String getName() {
+        return name;
+    }
+
+    public int getId() {
+        return id;
+    }
+}
