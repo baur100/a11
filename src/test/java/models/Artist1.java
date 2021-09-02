ff61c59af85ca3aa21c9c05576cace03c799e954
Joseph Jen <jjen.mis@gmail.com>
2021-09-01 21:40:09 -0500

    Created Artist1.java

---
+++ src/test/java/models/Artist1.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Artist1 {
+    private int number;
+    private String name;
+
+    public Artist1(int number, String name) {
+        this.number = number;
+        this.name = name;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
