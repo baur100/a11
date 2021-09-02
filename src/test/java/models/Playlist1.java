04e369f63998ea9b497b02c13a885814b12f1054
Joseph Jen <jjen.mis@gmail.com>
2021-09-01 21:42:06 -0500

    Created Playlist1.java

---
+++ src/test/java/models/Playlist1.java
@@ -0,0 +1,19 @@
+package models;
+
+public class Playlist1 {
+    private int id;
+    private String name;
+
+    public Playlist1(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
